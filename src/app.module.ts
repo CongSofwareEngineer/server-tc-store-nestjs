97217ed09cb9ca27ee702c7aaa37e9e7f9724961
HoDienCong <hodiencong2000@gmail.com>
2024-07-07 12:27:08 +0700

    example build

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,29 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { DataBaseName } from './common/mongoDB';
-import { UserModule } from './module/user/user.module';
-import { TypeProductModule } from './module/typeProduct/typeProduct.module';
-import { ProductModule } from './module/production/product.module';
+import { CarModule } from './module/cartUser/cart.module';
+
+// import { MongooseModule } from '@nestjs/mongoose';
+// import { DataBaseName } from './common/mongoDB';
+// import { UserModule } from './module/user/user.module';
+// import { TypeProductModule } from './module/typeProduct/typeProduct.module';
+// import { ProductModule } from './module/production/product.module';
 
 @Module({
   imports: [
-    ProductModule,
-    UserModule,
-    TypeProductModule,
+    CarModule,
+    // ProductModule,
+    // UserModule,
+    // TypeProductModule,
     ConfigModule.forRoot({
       envFilePath: '.env.local',
     }),
-    MongooseModule.forRoot(
-      `mongodb+srv://${process.env.USER_NAME_MONGO}:${process.env.PASSWORD_MONGO}@tc-store-admin.mpkyxqj.mongodb.net/?retryWrites=true&w=majority&appName=tc-store-admin`,
-      {
-        dbName: DataBaseName,
-        enableUtf8Validation: true,
-      },
-    ),
+    // MongooseModule.forRoot(
+    //   `mongodb+srv://${process.env.USER_NAME_MONGO}:${process.env.PASSWORD_MONGO}@tc-store-admin.mpkyxqj.mongodb.net/?retryWrites=true&w=majority&appName=tc-store-admin`,
+    //   {
+    //     dbName: DataBaseName,
+    //     enableUtf8Validation: true,
+    //   },
+    // ),
   ],
 })
 export class AppModule {}
