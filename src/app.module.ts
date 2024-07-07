ff1c2a556a879dfb6acc5a597d8dae704d2f9c3d
HoDienCong <hodiencong2000@gmail.com>
2024-07-07 12:30:08 +0700

    fix build

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CarModule } from './module/cartUser/cart.module';
 
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { DataBaseName } from './common/mongoDB';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DataBaseName } from './common/mongoDB';
 // import { UserModule } from './module/user/user.module';
 // import { TypeProductModule } from './module/typeProduct/typeProduct.module';
 // import { ProductModule } from './module/production/product.module';
@@ -17,13 +17,13 @@ import { CarModule } from './module/cartUser/cart.module';
     ConfigModule.forRoot({
       envFilePath: '.env.local',
     }),
-    // MongooseModule.forRoot(
-    //   `mongodb+srv://${process.env.USER_NAME_MONGO}:${process.env.PASSWORD_MONGO}@tc-store-admin.mpkyxqj.mongodb.net/?retryWrites=true&w=majority&appName=tc-store-admin`,
-    //   {
-    //     dbName: DataBaseName,
-    //     enableUtf8Validation: true,
-    //   },
-    // ),
+    MongooseModule.forRoot(
+      `mongodb+srv://hodiencong2000:diencong12c5@tc-store-admin.mpkyxqj.mongodb.net/?retryWrites=true&w=majority&appName=tc-store-admin`,
+      {
+        dbName: 'tc-store',
+        enableUtf8Validation: true,
+      },
+    ),
   ],
 })
 export class AppModule {}
